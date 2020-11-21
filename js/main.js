edfc419bf3d194044d99d61a5f34aa1697fba256
Owner <owner@Owners-MacBook-Pro.local>
2020-11-21 11:40:13 -0500

    Fleshing out components

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-import * as app from './app.js';
-import {Component} from './components/component.js'
+import * as app from './utilities/app_v2.js';
+import { lookup } from './utilities/registry.js'
 // import {ArrayVisualizer} from './js/features/array-visualizer.js';
 // import {ArrayVisualizer} from './js/features/array-visualizer.js';
 
 
 
 document.addEventListener('DOMContentLoaded', function() {
-        app.init('.app');
-        app.render();
+        // app.init('.app');
+        // app.render();
+        var topNav = lookup('TopNav');
+        var sortingVisualizer = lookup('SortingVisualizer');
+        var bottomNav = lookup('BottomNav');
 
-        var c = new Component();
-        window.c = c;
+        var banner = lookup('Banner');
+        var dialog = lookup('Dialog');
+        var exampleFeature = lookup('ExampleFeature');
+
+        window.ex = exampleFeature;
 });
 
